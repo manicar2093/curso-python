45248dbbd226fabc26816eb6f151b7f03dc129cb
Manuel Carbajal <manicar2093@gmail.com>
2022-04-26 10:48:57 -0500

    chore: Add fourth lesson

---
+++ 4.operadores_aritmeticos/main.py
@@ -0,0 +1,82 @@
+# Operadores aritmeticos
+
+# Suma
+numero_1 = 60
+numero_2 = 20
+
+# print(60 + 20)
+print(numero_1 + numero_2)
+
+temperatura_1 = 21.1
+temperatura_2 = 9.2
+
+# print(21.1 + 9.2)
+print(temperatura_1 + temperatura_2)
+
+nombre = "Erick"
+apellido = "Jimenez"
+
+# print("Erick" + "Jimenez")
+print(nombre + apellido + "jajaja" + "xD" + ":P")
+
+# Resta
+
+numero_1 = 60
+numero_2 = 20
+
+# print(60 - 20)
+print(numero_1 - numero_2)
+
+temperatura_1 = 21.1
+temperatura_2 = 9.2
+
+# print(21.1 - 9.2)
+print(temperatura_1 - temperatura_2)
+
+nombre = "Erick"
+apellido = "Jimenez"
+
+# NO SE PUEDE HACER RESTA CON str
+
+# Multiplicacion
+
+numero_1 = 60
+numero_2 = 20
+
+# print(60 * 20)
+print(numero_1 * numero_2)
+
+temperatura_1 = 21.1
+temperatura_2 = 9.2
+
+# print(21.1 + 9.2)
+print(temperatura_1 * temperatura_2)
+
+nombre = "Erick"
+
+# print("Erick" * 5)
+print(nombre * 5)
+
+# Division
+
+numero_1 = 60
+numero_2 = 20
+
+# print(60 / 20)
+print(numero_1 / numero_2)
+
+temperatura_1 = 21.1
+temperatura_2 = 9.2
+
+# print(21.1 / 9.2)
+print(temperatura_1 / temperatura_2)
+
+# 1. Parentesis
+# 2. Potencias y Raices
+# 3. Multiplicacion y Division
+# 4. Suma y Resta
+print(2 - 4 + 5 * 2 / 2)
+# print(2 - 4 + 10 / 2)
+# print(2 - 4 + 5)
+# print(-2 + 5)
+# print(3.0)
