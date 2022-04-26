b7ff65e35b37a3d87430d202ba302191b121d4e3
Manuel Carbajal <manicar2093@gmail.com>
2022-04-26 10:48:39 -0500

    chore: Add third lesson

---
+++ 3.tipos_de_datos/main.py
@@ -0,0 +1,27 @@
+# Tipos de datos en python
+
+# Enteros o Integers
+# 1, 2, 89
+edad:int = 18
+
+# Caracter o Character
+# a, b, c
+inicial:str = 'c'
+
+# Punto flotante o Float
+# 18.4, 6.09
+temperatura:float = 21.1
+
+# Cadenas de texto o Strings
+saludo:str = "Hola, mundo"
+
+# Booleans
+# True False
+es_divertido:bool = False
+tiene_dinero:bool = True
+
+# La lista:
+# int: Integers
+# float: Punto flotante
+# str: Cadenas de texto
+# bool: Buleanos
