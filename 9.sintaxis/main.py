affc9e9a369c5abc95199a6789406a60568e9e54
Manuel Carbajal <manicar2093@gmail.com>
2022-04-26 20:17:07 -0500

    chore: Add sintaxis lesson

---
+++ 9.sintaxis/main.py
@@ -0,0 +1,25 @@
+# Palabras reservadas
+
+# Estas no las podemos usar como nombres de variables
+
+"""
+    - if
+    - else
+    - elif
+    - in
+    - with
+    - for
+    - while
+    - or
+    - and
+    - continue
+    - break
+    - ...
+"""
+
+# if = "una variable invalida :("  # Esto nos da error
+es_verdad = "esto es verdadero"
+
+# Bloques de código
+if True: # Si esto se cumple se ejecutara el código que está "dentro"
+    print("Esto siempre es verdad :D") # El bloque se identifica con la identación que puede ser una tabulación o 4 espacios
